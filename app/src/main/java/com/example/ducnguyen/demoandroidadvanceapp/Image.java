b44b40bb6b875e8d3788d5c4f829d9b87405fcba
Nguyen Van Duc C <nguyen.van.ducc@framgia.com>
2018-08-17 15:01:30 +0700

    load image from storage

---
+++ app/src/main/java/com/example/ducnguyen/demoandroidadvanceapp/Image.java
@@ -0,0 +1,17 @@
+package com.example.ducnguyen.demoandroidadvanceapp;
+
+public class Image {
+    private String mData;
+
+    public Image(String data) {
+        mData = data;
+    }
+
+    public String getData() {
+        return mData;
+    }
+
+    public void setData(String data) {
+        mData = data;
+    }
+}
--- app/src/main/java/com/example/ducnguyen/demoandroidadvanceapp/ImageAdapter.java
+++ app/src/main/java/com/example/ducnguyen/demoandroidadvanceapp/ImageAdapter.java
@@ -1,5 +1,7 @@
 package com.example.ducnguyen.demoandroidadvanceapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -7,12 +9,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
-    private int[] mImagesId;
-    private int mLayoutId;
+import java.io.File;
+import java.util.List;
 
-    public ImageAdapter(int[] imagesId, int layoutId) {
-        this.mImagesId = imagesId;
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
+    private List<Image> mImages;
+    private int mLayoutId;
+    private static int IMAGE_WIDTH = 108;
+    private static int IMAGE_HEIGHT = 190;
+
+    public ImageAdapter(List<Image> images, int layoutId) {
+        this.mImages = images;
         this.mLayoutId = layoutId;
     }
 
@@ -26,12 +33,19 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> 
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.mImage.setImageResource(mImagesId[position]);
+        File imageFile = new File(mImages.get(position).getData());
+        if (imageFile.exists()) {
+            holder.mImage.setImageBitmap(decodeSampledBitmapFromResource(
+                    imageFile.getAbsolutePath()
+                    , IMAGE_WIDTH
+                    , IMAGE_HEIGHT
+            ));
+        }
     }
 
     @Override
     public int getItemCount() {
-        return mImagesId == null ? 0 : mImagesId.length;
+        return mImages == null ? 0 : mImages.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -41,5 +55,30 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> 
             super(itemView);
             mImage = itemView.findViewById(R.id.image_hero);
         }
+    }
+
+    private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        int height = options.outHeight;
+        int width = options.outWidth;
+        int inSampleSize = 1;
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
+    }
+
+    private Bitmap decodeSampledBitmapFromResource(String pathName, int reqWidth, int reqHeight) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(pathName, options);
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(pathName, options);
     }
 }
